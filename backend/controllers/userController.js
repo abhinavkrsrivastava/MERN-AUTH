d5bd553781882c5b3453bc3c901d34c6c9b83f50
Abhinav Kumar Srivastava <abhikrsrivastava01@gmail.com>
2024-05-04 19:37:36 +0530

    4th May 2024

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,14 @@
+import asyncHandler from "express-async-handler";
+
 //@Description = Auth User/ set token
 //route = POST /api/user/auth
 //@access Public
 
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+    res.status(401);
+    throw new Error('Something went wrong');
+
   res.status(200).json({ message: "Auth user" });
-};
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import Express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -11,5 +12,8 @@ const app = Express();
 app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => res.send("Server is ready"));
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is started on port ${port}`));
